b65dac641cd5e6e8e1cdcacbac03629a6393ffe0
alaJream <jeremy.gonzales@students.dominican.edu>
2022-02-28 01:33:26 -0800

    added footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Outlet } from 'react-router-dom'
 import './App.css';
 import Title from './components/Title/Title';
+import Footer from './Footer/Footer';
 
 function App() {
   return (
     <div>
       <Title />
       <Outlet />
+      <Footer />
     </div>
   );
 }
